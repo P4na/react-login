e4212c235315f6915047398851ca6d0b20f46879
P4na <Vip_P4na@protonmail.com>
2022-12-04 17:11:02 +0100

    recuperato files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import About from './components/About';
-import Home from './components/Home';
-import Error from './components/Error'
-import SharedLayout from './components/SharedLayout';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SharedLayout></SharedLayout>}>
-        <Route index element={<Home/>} />
-        <Route path='about' element={<About/>} />
-        <Route path='*' element={<Error />} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <main className="App">
+      <Login/>
+    </main>
   );
 }
 
--- src/components/Error.js
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-    return (
-    <>
-    pagina non trovata
-    
-    </>)
-}
-
-export default Error
